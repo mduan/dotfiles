e2271f3bb114a57839f3867ea66eebfb9e2ccf8e
mduan <duan.mack@gmail.com>
2012-05-27 07:26:08 +0000

    update

--- setup.sh
+++ setup.sh
@@ -14,19 +14,21 @@ read -p "This script will overwrite dotfiles in your home directory. Continue? [
 #   export TERM='xterm-color'
 # fi
 
-#sudo apt-get install zsh
+sudo apt-get install build-essential
+
+sudo apt-get install zsh
 sudo chsh -s /bin/zsh
 
-#sudo apt-get install git-core
-#sudo apt-get install tmux
-#sudo apt-get install vim
-#sudo apt-get install ack-grep
+sudo apt-get install git-core
+sudo apt-get install tmux
+sudo apt-get install vim
+sudo apt-get install ack-grep
 # TODO: should be doing this with an array
-sh -c 'rm -rf ~/.dotfiles/ && git clone git://github.com/mduan/dotfiles.git ~/.dotfiles/ && ln -sf ~/.dotfiles/.zshrc ~/.zshrc && ln -sf ~/.dotfiles/.vimrc ~/.vimrc && ln -sf ~/.dotfiles/.gvimrc ~/.gvimrc && rm -rf ~/.vim && ln -sf ~/.dotfiles/.vim ~/.vim && ln -sf ~/.dotfiles/.gitconfig ~/.gitconfig && ln -sf ~/.dotfiles/.gitignore_global ~/.gitconfig && ln -sf ~/.dotfiles/.ackrc ~/.ackrc && ln -sf ~/.dotfiles/.tmux.conf ~/.tmux.conf'
+sh -c 'rm -rf ~/.dotfiles/ && git clone git://github.com/mduan/dotfiles.git ~/.dotfiles/ && ln -sf ~/.dotfiles/.zshrc ~/.zshrc && ln -sf ~/.dotfiles/.vimrc ~/.vimrc && ln -sf ~/.dotfiles/.gvimrc ~/.gvimrc && rm -rf ~/.vim && ln -sf ~/.dotfiles/.vim ~/.vim && ln -sf ~/.dotfiles/.gitconfig ~/.gitconfig && ln -sf ~/.dotfiles/.gitignore_global ~/.gitignore_global && ln -sf ~/.dotfiles/.ackrc ~/.ackrc && ln -sf ~/.dotfiles/.tmux.conf ~/.tmux.conf'
 
 sh -c 'mkdir -p ~/.zsh && rm -rf ~/.zsh/git-prompt && git clone https://github.com/olivierverdier/zsh-git-prompt.git ~/.zsh/git-prompt'
 
-#sudo apt-get install ruby-dev # need it for mkmf needed by Command-T
+sudo apt-get install ruby-dev # need it for mkmf needed by Command-T
 sh -c 'rm -rf ~/.vim/bundle/vundle && git clone http://github.com/gmarik/vundle.git ~/.vim/bundle/vundle/ && vim -c "silent BundleInstall" -c "silent helptags ~/.vim/bundle/vundle/doc" -c "silent qa!" && cd ~/.vim/bundle/Command-T/ruby/command-t && ruby extconf.rb && make && cd -'
 
 # TODO: looks like autojump is in official repo, so change to use apt-get install
