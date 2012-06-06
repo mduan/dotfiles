c979c5f370506241d35eed579cff2a917e2eae3a
mduan <duan.mack@gmail.com>
2012-06-06 00:11:10 -0700

    update

--- setup.sh
+++ setup.sh
@@ -24,16 +24,18 @@ sudo apt-get install git-core
 sudo apt-get install tmux
 sudo apt-get install vim
 sudo apt-get install ack-grep
+
 # TODO: should be doing this with an array
 sh -c 'rm -rf ~/.dotfiles/ && git clone git://github.com/mduan/dotfiles.git ~/.dotfiles/ && ln -sf ~/.dotfiles/.zshrc ~/.zshrc && ln -sf ~/.dotfiles/.vimrc ~/.vimrc && ln -sf ~/.dotfiles/.gvimrc ~/.gvimrc && rm -rf ~/.vim && ln -sf ~/.dotfiles/.vim ~/.vim && ln -sf ~/.dotfiles/.gitconfig ~/.gitconfig && ln -sf ~/.dotfiles/.gitignore_global ~/.gitignore_global && ln -sf ~/.dotfiles/.ackrc ~/.ackrc && ln -sf ~/.dotfiles/.tmux.conf ~/.tmux.conf'
 
 sh -c 'mkdir -p ~/.zsh && rm -rf ~/.zsh/git-prompt && git clone https://github.com/olivierverdier/zsh-git-prompt.git ~/.zsh/git-prompt'
 
+sudo apt-get install ruby
 sudo apt-get install ruby-dev # need it for mkmf needed by Command-T
 sh -c 'rm -rf ~/.vim/bundle/vundle && git clone http://github.com/gmarik/vundle.git ~/.vim/bundle/vundle/ && vim -c "silent BundleInstall" -c "silent helptags ~/.vim/bundle/vundle/doc" -c "silent qa!" && cd ~/.vim/bundle/Command-T/ruby/command-t && ruby extconf.rb && make && cd -'
 
-# TODO: looks like autojump is in official repo, so change to use apt-get install
-sh -c 'rm -rf /tmp/git_autojump/ && cd /tmp && wget https://github.com/downloads/joelthelion/autojump/autojump_v20.tar.gz && cd /tmp/autojump_v20 && chmod a+x install.sh && ./install.sh --local --zsh'
+sudo apt-get install autojump
+#sh -c 'rm -rf /tmp/git_autojump/ && cd /tmp && wget https://github.com/downloads/joelthelion/autojump/autojump_v20.tar.gz && cd /tmp/autojump_v20 && chmod a+x install.sh && ./install.sh --local --zsh'
 
 # any additional packages that might be worth installing
 #sudo apt-get install vim-gnome; # contains more extensions than vim package (i.e. clipboard support)
